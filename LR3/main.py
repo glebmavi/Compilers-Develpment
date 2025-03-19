78484b15fcfb1827ef9295df8ab3930a56b9f0b0
Gleb Malikov Villegas ^-^ <44012947+glebmavi@users.noreply.github.com>
2025-03-19 16:40:37 +0300

    lr3

--- LR3/main.py
+++ LR3/main.py
@@ -1,3 +1,22 @@
+import logging
+import colorlog
+
+logger = logging.getLogger()
+
+handler = logging.StreamHandler()
+
+formatter = colorlog.ColoredFormatter(
+    "%(log_color)s%(message)s",
+    log_colors={
+        'INFO': 'green',
+        'DEBUG': 'white',
+        'ERROR': 'red',
+    }
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
 grammar = {
     'S': {
         'b': ['A', 'A', 'B', 'C']
@@ -33,25 +52,28 @@ def parse(input_tokens):
     index = 0
 
     while stack:
+        logging.debug(f"Стек: {''.join(stack)}, вход: {''.join(input_tokens[index:])}, позиция: {index + 1} ")
         top = stack.pop()
         current = input_tokens[index]
         if top == current == '$':
-            print("Разбор завершён успешно!")
+            logging.info("Разбор завершён успешно!")
             return True
         # Если верх стека – терминал
         if top in ['a','b','c','d','$']:
             if top == current:
                 index += 1
+                logging.debug(f"Совпадение терминала '{top}'")
             else:
-                print(f"Ошибка на позиции {index + 1}: ожидалось '{top}', а найдено '{current}'")
+                logging.error(f"Ошибка на позиции {index + 1}: ожидалось '{top}', а найдено '{current}'")
                 return False
         else:
             # Нетерминал – ищем правило по таблице
             production = grammar.get(top, {}).get(current)
             if not production:
-                print(f"Ошибка на позиции {index + 1}: нет правила для '{top}' при токене '{current}'")
+                logging.error(f"Ошибка на позиции {index + 1}: нет правила для '{top}' при токене '{current}'")
                 return False
             if production != ['ε']:
+                logging.debug(f"Применено правило {top} -> {''.join(production)}")
                 # Правило записано как список символов – добавляем их в стек в обратном порядке
                 for symbol in reversed(production):
                     stack.append(symbol)
@@ -62,7 +84,7 @@ tokens = list('bdbdbddcc')
 parse(tokens) # правильно
 tokens = list('adbabddacc')
 parse(tokens) # неправильно
-tokens = list('bddbadbad')
+tokens = list('bdbdbccbad')
 parse(tokens) # неправильно
 tokens = list('bbabcadbdbbddaa')
 parse(tokens) # правильно
