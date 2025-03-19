6e17054b4c7ebff3189161eecfd0d5db2827a508
Gleb Malikov Villegas ^-^ <44012947+glebmavi@users.noreply.github.com>
2025-03-19 16:26:51 +0300

    lr3 temp

--- LR3/main.py
+++ LR3/main.py
@@ -1,29 +1,35 @@
-
 grammar = {
-    'S': { 'b': ['A', 'A', 'B', 'C'] },
-    'A': { 'b': ['b', "A'"] },
-    "A'": {
-         'a': ["a", "A'"],
-         'b': ["A", "a", "A'"],
-         'c': ['ε']
+    'S': {
+        'b': ['A', 'A', 'B', 'C']
     },
-    'B': { 'b': ['b', 'X'] },
+    'A': {
+        'b': ['b', "A'"]
+    },
+    "A'": {
+        'a': ["a", "A'"],
+        'b': ["b", "A'"],
+        'c': ["c", "a", "A'"],
+        'd': ["d"],
+    },
+    'B': {
+        'b': ['b', 'X']
+    },
     'X': {
-         'a': ['C'],
-         'b': ['A'],
-         'c': ['C'],
-         '$': ['ε']
+        'a': ['a', 'A'],
+        'b': ['B'],
+        'c': ['c', 'C'],
+        'd': ['d', 'd']
     },
     'C': {
-         'a': ['aa'],
-         'b': ['bb'],
-         'c': ['cc']
+        'a': ['a', 'a'],
+        'b': ['b', 'b'],
+        'c': ['c', 'c']
     }
 }
 
 def parse(input_tokens):
     input_tokens.append('$')
-    stack = ['$','S']
+    stack = ['$', 'S']
     index = 0
 
     while stack:
@@ -33,24 +39,30 @@ def parse(input_tokens):
             print("Разбор завершён успешно!")
             return True
         # Если верх стека – терминал
-        if top in ['a','b','c','$']:
+        if top in ['a','b','c','d','$']:
             if top == current:
                 index += 1
             else:
-                print(f"Ошибка: ожидалось '{top}', а найдено {current}")
+                print(f"Ошибка на позиции {index + 1}: ожидалось '{top}', а найдено '{current}'")
                 return False
         else:
             # Нетерминал – ищем правило по таблице
             production = grammar.get(top, {}).get(current)
             if not production:
-                print(f"Ошибка: нет правила для {top} при токене {current}")
+                print(f"Ошибка на позиции {index + 1}: нет правила для '{top}' при токене '{current}'")
                 return False
             if production != ['ε']:
-                # Правило записано как список символов – добавляем в стек в обратном порядке
+                # Правило записано как список символов – добавляем их в стек в обратном порядке
                 for symbol in reversed(production):
                     stack.append(symbol)
     return False
 
 
-tokens = ['b', 'b', 'c', 'b', 'a', 'a']
-parse(tokens)
+tokens = list('bdbdbddcc')
+parse(tokens) # правильно
+tokens = list('adbabddacc')
+parse(tokens) # неправильно
+tokens = list('bddbadbad')
+parse(tokens) # неправильно
+tokens = list('bbabcadbdbbddaa')
+parse(tokens) # правильно
